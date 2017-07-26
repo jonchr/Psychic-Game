e12e28cd43d27bf0e18bffb383289595d76a673c
Jon <jonathan.lee.christensen@gmail.com>
2017-07-25 22:20:33 -0400

    still no css, but more snark

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,6 +19,7 @@
 
 	//Variable for the number of attempts a player has
 	//Someone editing the number of attempts only needs to edit the number here below
+	//Must be greater than 0;
 	var maxAttempts = 9;
 
 
@@ -33,40 +34,90 @@
 		//Comes up with a random new letter to be guessed
 		theLetter = alphabet[Math.floor(Math.random()*25)];
 
+		//Sets or updates the displayed win/loss record
+		updateWins("Wins: " + wincounter);
+		updateLosses("Losses: " + losscounter);
+
 		//Resets the guess attempts for the new game
-		document.getElementById("numGuesses").innerHTML = "Guesses left: " + maxAttempts;
+		updateGuesses("Guesses left: " + maxAttempts);
 		
 		//Clears the letters guessed for the new game
-		document.getElementById("guesses").innerHTML = "Letters guessed thus far:";
+		updatePrevLetters("Letters guessed thus far:");
 	}
 
 	function win() {
 
-		//Increases the number of wins recorded and displayed, and calls for a new game
+		//Increases the number of wins recorded, and calls for a new game
 		wincounter++;
-		document.getElementById("wins").innerHTML = "Wins: " + wincounter;
 		newGame();
+
+		//Gives different messages based on how much you've won
+		if (wincounter > 5) {
+			updateBoard("Wow! You're really good at this :) !");
+		}
+		else if (wincounter > 1) {
+			updateBoard("You won again! Keep going?");
+		}
+		else {
+			updateBoard("You won! Play again?");
+		}
 
 	}
 
 	function lose() {
 
-		//Increases the number of losses recorded and displayed, and calls for a new game
+		//Increases the number of losses recorded, and calls for a new game
 		losscounter++;
-		document.getElementById("losses").innerHTML = "Losses: " + losscounter;
 		newGame();
+
+		//Gives different messages based on how much you've won
+		if (losscounter > 5) {
+			updateBoard("As your resident psychic website, I must advise that you stop while you're behind");
+		}
+		else if (losscounter > 1) {
+			updateBoard("You lost again! Keep at it?");
+		}
+		else {
+			updateBoard("You lost :( But I'm already thinking of a new letter!");
+		}
 	}
 
 	function update() {
 
 		//Updates the guess attempts remaining in the game
-		document.getElementById("numGuesses").innerHTML = "Guesses left: " +  (maxAttempts - prevGuessedLetters.length);
+		updateGuesses("Guesses left: " +  (maxAttempts - prevGuessedLetters.length));
 		
 		//Updates the log of letters guessed by the player this game
-		document.getElementById("guesses").innerHTML = "Letters guessed thus far: " + prevGuessedLetters.join();
+		updatePrevLetters("Letters guessed thus far: " + prevGuessedLetters.join());
 
+		if (maxAttempts - prevGuessedLetters.length === 1) {
+			updateBoard("You have one attempt left. Make the most of it!");
+		}
+		else {
+			updateBoard("You guessed " + guessedLetter + "! It's incorrect.");
+		}
 	}
 
+	//Functions to update HTML text on the page
+	function updateWins (winMessage) {
+		document.getElementById("wins").innerHTML = winMessage;
+	}
+
+	function updateLosses (lossMessage) {
+		document.getElementById("losses").innerHTML = lossMessage;
+	}
+
+	function updateGuesses (guessMessage) {
+		document.getElementById("numGuesses").innerHTML = guessMessage;
+	}
+
+	function updatePrevLetters (prevMessage) {
+		document.getElementById("prevLetters").innerHTML = prevMessage;
+	}
+
+	function updateBoard (boardMessage) {
+		document.getElementById("updateBoard").innerHTML = boardMessage;
+	}
 
 
 // FUNCTION EXECUTION
@@ -90,18 +141,22 @@
 
 				//Processes if the guess was correct and responds accordingly
 				if (guessedLetter === theLetter) {
-					console.log("You win!");
 					win();
 				}
 				else if (prevGuessedLetters.length === maxAttempts) {
-					console.log("You lost");
 					lose();
 				}
 				else {
-					console.log("Updating...");
 					update();
 				}
-				
 			}
+			else {
+				//Adds snarky comment if letter was already guessed
+				updateBoard("You already guessed " + guessedLetter + "!");
+			}
+		}
+		else {
+			//Adds snarky comment if button pressed isn't a letter
+			updateBoard("Please. press. a letter...");
 		}
 	};
